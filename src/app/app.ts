165bc772661a2822fb9b1da9e1b5bd837be2d291
Michelle Stegeman <m.stegeman@vanlanschotkempen.com>
2025-10-22 17:46:30 +0200

    feat: refactor app layout and styles, remove unused code and enhance background gradients

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,41 +7,9 @@ import { combineLatest, map, Observable, shareReplay } from 'rxjs';
 
 import { CharacterCard } from './components/character-card/character-card';
 
-interface location {
-  name: string;
-  url: string;
-} 
-
-export interface Character {
-  created: string;
-  episode: string[];
-  gender: string;
-  id: number;
-  image: string;
-  location: location;
-  name: string;
-  origin: location;
-  species: string;
-  status: string;
-  type: string;
-  url: string;
-}
-
-interface Info {
-  count: number;
-  pages: number;
-  next: string | null;
-  prev: string | null;
-}
-
-interface ApiResponse {
-  info: Info;
-  results: Character[];
-}
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, CommonModule, RouterModule, CharacterCard],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -50,40 +18,40 @@ export class App {
   protected readonly title = 'Rick and Morty Wiki';
   protected readonly searchBar = new FormControl();
   protected readonly searchString$: Observable<string> = this.searchBar.valueChanges;
-  protected results$!: Observable<Character[]>;
-  protected info$!: Observable<Info>;
+  // protected results$!: Observable<Character[]>;
+  // protected info$!: Observable<Info>;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.searchString$.subscribe(value => {
-      console.log(value);
-    }); 
+  //   this.searchString$.subscribe(value => {
+  //     console.log(value);
+  //   }); 
 
-    const apiResponse$ = this.http.get<ApiResponse>('https://rickandmortyapi.com/api/character').pipe(shareReplay());
+  //   const apiResponse$ = this.http.get<ApiResponse>('https://rickandmortyapi.com/api/character').pipe(shareReplay());
 
-    apiResponse$.subscribe(result => {
-      console.log(result);
-    });
+  //   apiResponse$.subscribe(result => {
+  //     console.log(result);
+  //   });
 
-    this.results$ = apiResponse$.pipe(
-      map(response => response.results)
-    );
+  //   this.results$ = apiResponse$.pipe(
+  //     map(response => response.results)
+  //   );
 
-    this.info$ = apiResponse$.pipe(
-      map(response => response.info)
-    );
+  //   this.info$ = apiResponse$.pipe(
+  //     map(response => response.info)
+  //   );
 
-    this.results$ = combineLatest([this.results$, this.searchString$]).pipe(
-      map(([results, search]) => 
-        results.filter(character => 
-          character.name.toLowerCase().includes((search ?? '').toLowerCase())
-        )
-      )
-    );
+  //   this.results$ = combineLatest([this.results$, this.searchString$]).pipe(
+  //     map(([results, search]) => 
+  //       results.filter(character => 
+  //         character.name.toLowerCase().includes((search ?? '').toLowerCase())
+  //       )
+  //     )
+  //   );
 
-  }
+  // }
 
-  onSearch() {
+  // onSearch() {
   }
 }
