98dcafe3723c737b0f4b40a9067df713c387dd01
Michelle Stegeman <m.stegeman@vanlanschotkempen.com>
2025-11-13 08:15:07 +0100

    refactor: remove unused search functionality and related observables

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { combineLatest, map, Observable, shareReplay } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,42 +14,7 @@ import { combineLatest, map, Observable, shareReplay } from 'rxjs';
 
 export class App {
   protected readonly title = 'Rick and Morty Wiki';
-  protected readonly searchBar = new FormControl();
-  protected readonly searchString$: Observable<string> = this.searchBar.valueChanges;
-  // protected results$!: Observable<Character[]>;
-  // protected info$!: Observable<Info>;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-  //   this.searchString$.subscribe(value => {
-  //     console.log(value);
-  //   }); 
-
-  //   const apiResponse$ = this.http.get<ApiResponse>('https://rickandmortyapi.com/api/character').pipe(shareReplay());
-
-  //   apiResponse$.subscribe(result => {
-  //     console.log(result);
-  //   });
-
-  //   this.results$ = apiResponse$.pipe(
-  //     map(response => response.results)
-  //   );
-
-  //   this.info$ = apiResponse$.pipe(
-  //     map(response => response.info)
-  //   );
-
-  //   this.results$ = combineLatest([this.results$, this.searchString$]).pipe(
-  //     map(([results, search]) => 
-  //       results.filter(character => 
-  //         character.name.toLowerCase().includes((search ?? '').toLowerCase())
-  //       )
-  //     )
-  //   );
-
-  // }
-
-  // onSearch() {
-  }
 }
